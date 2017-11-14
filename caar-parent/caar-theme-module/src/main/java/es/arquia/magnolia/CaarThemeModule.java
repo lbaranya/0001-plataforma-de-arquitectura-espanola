6ea962d563dc92b28c9b0578f24a97d64f9ec1a7
David Caviedes Márquez <dcaviedes@atsistemas.com>
2017-11-14 12:35:29 +0100

    -Esqueleto del resto de proyectos contemplados en el DT

---
+++ caar-parent/caar-theme-module/src/main/java/es/arquia/magnolia/CaarThemeModule.java
@@ -0,0 +1,12 @@
+package es.arquia.magnolia;
+
+/**
+ * This class is optional and represents the configuration for the caar-theme-module module.
+ * By exposing simple getter/setter/adder methods, this bean can be configured via content2bean
+ * using the properties and node from <tt>config:/modules/caar-theme-module</tt>.
+ * If you don't need this, simply remove the reference to this class in the module descriptor xml.
+ */
+public class CaarThemeModule {
+    /* you can optionally implement info.magnolia.module.ModuleLifecycle */
+
+}
