1011332cba338a189dd1f694daa997ff0e0d309f
ATJDIAZP\jdiaz <jdiaz@atsistemas.com>
2017-11-21 13:35:19 +0100

    Modificación del pom de proyecto a versión 0.1-SNAPSHOT. Creación de la estructura de carpetas para las pruebas unitarias.

---
+++ caar-parent/caar-architecture-files-app/src/test/java/es/arquia/magnolia/setup/CaarArquitectureFilesAppVersionHandlerTest.java
@@ -0,0 +1,13 @@
+package es.arquia.magnolia.setup;
+
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
+
+public class CaarArquitectureFilesAppVersionHandlerTest {
+
+	@Test
+	public void shouldBeOneInstanceClass() {
+		assertNotNull(new CaarArquitectureFilesAppVersionHandler());
+	}
+	
+}
---
+++ caar-parent/caar-utils-module/src/test/java/es/arquia/magnolia/setup/CaarUtilsModuleVersionHandlerTest.java
@@ -0,0 +1,13 @@
+package es.arquia.magnolia.setup;
+
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
+
+public class CaarUtilsModuleVersionHandlerTest {
+
+	@Test
+	public void shouldBeOneInstanceClass() {
+		assertNotNull(new CaarUtilsModuleVersionHandler());
+	}
+	
+}
