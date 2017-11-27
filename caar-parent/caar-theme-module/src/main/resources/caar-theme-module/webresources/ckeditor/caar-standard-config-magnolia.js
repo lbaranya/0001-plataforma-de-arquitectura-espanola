95dfa25eb77b0734a8c1f370cee74c3a13ac8a8d
jmruiz <jmruiz@atsistemas.com>
2017-11-27 13:59:13 +0100

    Eliminado archivo innecesario de configuración de CKEditor

--- caar-parent/caar-theme-module/src/main/resources/caar-theme-module/webresources/ckeditor/caar-standard-config-magnolia.js
+++
@@ -1,5 +0,0 @@
-CKEDITOR.editorConfig = function( config ) {
-	config.forceEnterMode = true;
-	config.format_div = {element : 'div', attributes : { 'class' : 'contenido' }};
-	config.format_p = {element : 'p', attributes : { 'class' : 'autor' }};
-}
