bb9e03f2addb6612a50d2ba96c80019d3f7ec281
David Caviedes Márquez <dcaviedes@atsistemas.com>
2017-12-15 13:29:02 +0100

    -Obtención de parámetros de configuración de email desde JCR, paso de parámetros a comando e internacionalización de textos de email -Bootstrap de configuración del comando y de la template de email

--- caar-parent/caar-utils-module/src/main/java/es/arquia/magnolia/commands/RequestTranslationCommand.java
+++ caar-parent/caar-utils-module/src/main/java/es/arquia/magnolia/commands/RequestTranslationCommand.java
@@ -8,14 +8,21 @@ import javax.inject.Inject;
 import info.magnolia.commands.CommandsManager;
 import info.magnolia.commands.impl.BaseRepositoryCommand;
 import info.magnolia.context.Context;
+import info.magnolia.i18nsystem.SimpleTranslator;
+import info.magnolia.module.mail.MailTemplate;
 
 public class RequestTranslationCommand extends BaseRepositoryCommand {
     
     /** The Constant TEMPLATE_PARAMETER_NAME. */
-    private static final String TEMPLATE_PARAMETER_NAME= "mailTemplate";
+    private static final String TEMPLATE_PARAM_NAME = "mailTemplate";
     
     /** The Constant DATA_PARAMETER_NAME. */
-    private static final String DATA_PARAMETER_NAME= "emailData";
+    private static final String DATA_PARAM_NAME = "emailData";
+    
+    /** Email data parameters names */
+    private static final String DATA_PATH_TO_TRANSLATABLE_NODE_PARAM_NAME = "pathToTranslatableNode";
+    
+    private static final String MAIL_PARAMETER_SEPARATOR = "\r\n";
 	
 	private final CommandsManager commandsManager;
   
@@ -25,21 +32,44 @@ public class RequestTranslationCommand extends BaseRepositoryCommand {
     /** The mail command. */
     private String mailCommand;
     
+    private final SimpleTranslator i18n;
+    
     /** The mail template. */
     private String mailTemplate;
+    
+    private String subject;
+    private String from;
+    private String to;
 	
 	@Inject
-	public RequestTranslationCommand(final CommandsManager commandsManager) {
+	public RequestTranslationCommand(final CommandsManager commandsManager, final SimpleTranslator i18n) {
 		this.commandsManager = commandsManager;
+		this.i18n = i18n;
 	}
 
 	@Override
 	public boolean execute(Context context) throws Exception {
 
 		Map<String, Object> mailParameters = new HashMap<>();
-        mailParameters.put(TEMPLATE_PARAMETER_NAME, this.mailTemplate);
-        mailParameters.put(DATA_PARAMETER_NAME, this.getPath());
+		
+		// Node name that defines mail template in /modules/mail/config/templatesConfiguration
+        mailParameters.put(TEMPLATE_PARAM_NAME, this.mailTemplate);
         
+        // Mail parameters (to, from, etc) defined in a string with format name=value\r\nname=value
+	        StringBuffer sbParameters = new StringBuffer();
+	        sbParameters.append(MailTemplate.MAIL_TO + "=" + this.to);
+	        sbParameters.append(MAIL_PARAMETER_SEPARATOR);
+	        sbParameters.append(MailTemplate.MAIL_FROM + "=" + this.from);
+	        sbParameters.append(MAIL_PARAMETER_SEPARATOR);
+	        sbParameters.append(MailTemplate.MAIL_SUBJECT + "=" + this.i18n.translate(this.subject));
+	    mailParameters.put(MailTemplate.MAIL_PARAMETERS, sbParameters.toString());
+        
+	    // Map that represents data to be inserted into template script
+		    Map<String, Object> dataParameters = new HashMap<>();
+		    dataParameters.put(DATA_PATH_TO_TRANSLATABLE_NODE_PARAM_NAME, this.getPath());
+        mailParameters.put(DATA_PARAM_NAME, dataParameters);
+        
+        // Command call
 		this.commandsManager.executeCommand(this.mailCommandCatalog, this.mailCommand, mailParameters);
 
 		return true;
@@ -69,6 +99,30 @@ public class RequestTranslationCommand extends BaseRepositoryCommand {
 
 	public void setMailTemplate(String mailTemplate) {
 		this.mailTemplate = mailTemplate;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
 	}
 
 }
--- caar-parent/caar-utils-module/src/main/resources/email/requestTranslation.ftl
+++ caar-parent/caar-utils-module/src/main/resources/email/requestTranslation.ftl
@@ -1,12 +1,12 @@
 <html>
     <head>
-        Solicitud de traducci&oacute;n
+        ${i18n['caar-utils-module.templates.email.header']!"Solicitud de traduccion"}
     </head>
     <body>
         <dl>
         
-        	[#if emailData?? && emailData?has_content]
-	        	<dt>Se ha solicitado la traducci&oacute;n del nodo: ${emailData}</dt>
+        	[#if emailData['pathToTranslatableNode']?? && emailData['pathToTranslatableNode']?has_content]
+	        	<dt>${i18n['caar-utils-module.templates.email.body']!"Se ha solicitado la traduccion de la noticia: "}${emailData['pathToTranslatableNode']}</dt>
 	        [/#if]
         </dl>
     </body>
