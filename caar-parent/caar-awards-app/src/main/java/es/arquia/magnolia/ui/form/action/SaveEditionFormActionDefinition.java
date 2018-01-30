5c55b51e233aaf5d01d5056a8f5f7255a4d6403b
Juan Miguel Ruiz Ladrón <jmruiz@atsistemas.com>
2018-01-30 12:56:02 +0100

    /CAAR0001-408: Auto stash before merge of "feature/CAAR0001-408" and "develop"

---
+++ caar-parent/caar-awards-app/src/main/java/es/arquia/magnolia/ui/form/action/SaveEditionFormActionDefinition.java
@@ -0,0 +1,11 @@
+package es.arquia.magnolia.ui.form.action;
+
+import info.magnolia.ui.api.action.ConfiguredActionDefinition;
+
+public class SaveEditionFormActionDefinition extends ConfiguredActionDefinition {
+
+    public SaveEditionFormActionDefinition() {
+        setImplementationClass(SaveEditionFormAction.class);
+    }
+
+}
